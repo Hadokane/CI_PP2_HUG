eef3654ecb935a0280e3ac9043c95821b9693f50
Hadokane <kanedaveo@gmail.com>
2022-12-11 13:30:45 +0000

    Add basic new game functionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,24 @@
 var btn1 = document.getElementById('btn1');
 var btn2 = document.getElementById('btn2');
 var btn3 = document.getElementById('btn3');
-var text = document.getElementById('comp-text');
+var cText = document.getElementById('comp-text');
+
+/* Initalises the game */
+let game = {
+    failScore: 10,
+    currentGame: [],
+    text: "THIS IS WORKING!",
+};
+
+/* Shows the current score on screen */
+function showScore () {
+    document.getElementById("score").innerText = game.failScore;
+}
+
+/* Adds 1 to the score */
+function updateScore (){
+    game.failScore++;
+}
 
 /**
  * Sets the innerText of var text to a callable function.
@@ -10,23 +27,50 @@ var text = document.getElementById('comp-text');
  * Updates the current game.text being displayed.
  *  */ 
 function updateText() {
-    text.innerText = game.text;
+    cText.innerText = game.text;
 };
 
-/* Initalises the game */
-let game = {
-    currentGame: [],
-    failScore: 10,
-    text: "THIS IS WORKING!",
-};
-
-/* Level 1 - Basic Aptitude Test */
-let levelOne = {
-    
+/* Resets settings when launching a new game. */
+function newGame(){
+    game.currentGame = [];
+    game.failScore = 0;
+    game.text = "Welcome New Candidate to the H.U.G. Protocol."
 }
 
-/* Calling updateText Function */
+/**Creates a username text field for the player to interact with.
+ * Saves the username on button click. 
+ * Removes the button now that data is set. 
+ */
+function levelOne(){
+    /* Creates an input box for the user to enter their username */
+    let nameBox = document.createElement("div");
+    /* Sets the innerHTML of the newly created div object */
+    nameBox.innerHTML = `
+        <label>Enter Your Name:</label>
+        <br>
+        <input type="text" id="userText">
+        <br>
+        <button type="button" id="nameButton" class="btn">submit</button>
+    `;
+    /* Appends it to the empty "user" div inside of the html file */
+    document.getElementById("user").append(nameBox);
+    /* Logs button press and sets the value of username */
+    document.getElementById("nameButton").onclick = function(){
+        let username = document.getElementById("userText");
+        /* Appends it to the empty "user" div inside of the html file */
+        document.getElementById("user").remove(nameBox);
+        /* Updates computer players text */
+        game.text = "Welcome" + $('#username') + ", enjoy the H.U.G. Protocol."
+        updateText();
+    };
+}
+
+/* Calling functions for test purposes */
 updateText();
+showScore();
+levelOne()
+
+/* Level 1 - Basic Aptitude Test */
 
 /* Exports functions for the test file to access */
-module.exports = { game, text, updateText, };
+module.exports = { game, updateText, newGame, };
