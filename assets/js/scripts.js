4b3f1557c738536f20f67cb6211e59a6a9943a14
Hadokane <kanedaveo@gmail.com>
2022-12-12 10:10:12 +0000

    Update username to be a global variable

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,7 @@ var btn1 = document.getElementById('btn1');
 var btn2 = document.getElementById('btn2');
 var btn3 = document.getElementById('btn3');
 var cText = document.getElementById('comp-text');
+var username;
 
 /* Initalises the game */
 let game = {
@@ -42,7 +43,6 @@ function newGame(){
     game.failScore = 12;
     game.text = "Welcome New Candidate to the H.U.G. Protocol."
 }
-
 /**Creates a username text field for the player to interact with.
  * Saves the username on button click. 
  * Removes the button now that data is set. 
@@ -65,8 +65,8 @@ function levelOne(){
     document.getElementById("nameButton").onclick = function(){
         /* Sets the value of userText to an object */
         let userString = document.getElementById("user-text").value;
-        /*Converts the above object into a string, wraps in a span */
-        let username = "<span id='username-style'>" + JSON.stringify(userString) + "</span>";
+        /* Converts the above object into a string, wraps it in a span */
+        username = "<span id='username-style'>" + JSON.stringify(userString) + "</span>";
         /* Appends it to the remove nameBox from the "user" div inside of the html file */
         document.getElementById("user").remove(nameBox);
         /* Updates computer players text and references the players username */
@@ -79,20 +79,20 @@ function levelOne(){
             await sleep(3000);
             game.text = "No matter " + username + " you'll have to do.";
             updateText();
-            // await sleep(3000);
-            // game.text = "Enjoy your initiation...";
-            // updateText();
-            // await sleep(3000);
-            // levelTwo();
+            await sleep(3000);
+            game.text = "Enjoy your initiation...";
+            updateText();
+            await sleep(3000);
+            levelTwo();
         }
         delayedGreeting()
-    }};
+    }}
 
 function levelTwo(){
-    game.currentGame = [];
-    game.failScore = 15;
-    game.text = "Welcome " + username + ", let's begin with a few simple questions.";
-};
+        game.currentGame = [];
+        game.failScore = 42;
+        game.text = "Your first test" + username + " during the H.U.G. Protocol.";
+}
 
 /* jQuery Test - recolouring buttons to test functionality */
 $(".btn").addClass("btn-red");
@@ -101,8 +101,6 @@ $(".btn").addClass("btn-red");
 levelOne();
 updateText();
 showScore();
-levelTwo();
-
 
 /* Level 1 - Basic Aptitude Test */
 
